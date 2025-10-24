e9c658aee0d6eec5d8450b1e5f06135e6614a74a
Irtiza-13 <haisibukhari@gmail.com>
2025-10-25 04:08:17 +0500

    Footer Update + Css Update

--- laravel/resources/views/partials/footer.blade.php
+++ laravel/resources/views/partials/footer.blade.php
@@ -1,15 +1,5 @@
-<!-- <footer>
-    <div class="footer-content">
-        <p>&copy; 2024 OmniPlay. All rights reserved.</p>
-    </div>
-</footer>
- -->
-
 <footer>
     <div class="footer-content">
         <p>🎯 &copy; 2024 OmniPlay. Level Up Your Gaming Experience! 🎯</p>
-        <div class="footer-links">
-            <a href="/about">About Us</a>
-        </div>
     </div>
 </footer>
